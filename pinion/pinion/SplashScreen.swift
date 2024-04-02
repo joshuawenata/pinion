6f6a7275d6d063f7f810b7d23eb361f1ea6e3241
Vincent <31364417+vcsrng@users.noreply.github.com>
2024-04-02 11:37:18 +0700

    Splash Screen and Idle Screen

--- pinion/pinion/SplashScreen.swift
+++ pinion/pinion/SplashScreen.swift
@@ -1,18 +1,50 @@
+
 //
-//  SplashScreen.swift
-//  pinion
+//  SplashView.swift
+//  Test1
 //
-//  Created by Vincent Saranang on 02/04/24.
+//  Created by Vincent Saranang on 01/04/24.
 //
 
 import SwiftUI
 
-struct SplashScreen: View {
+struct SplashView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack{
+                VStack{
+                    Image("Vector 2")
+                        .resizable()
+                        .frame(width: 120, height: 160)
+                    Image("Vector 3")
+                        .resizable()
+                        .frame(width: 28, height: 32)
+                }.padding(10)
+                
+                Text("PINION")
+                    .font(.largeTitle)
+                    .fontDesign(.rounded)
+                  .fontWeight(.heavy)
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(Color(red: 0.12, green: 0.7, blue: 0.65))
+                  .frame(width: 120, height: 32, alignment: .top)
+            }
+            Image("Vector 7")
+                .resizable()
+                .frame(width: 450, height: 600)
+                .rotationEffect(Angle(degrees: 112.97))
+                .position(x: 1500, y: 450)
+            Image("Vector 7-1")
+                .resizable()
+                .frame(width: 450, height: 600)
+                .rotationEffect(Angle(degrees: -71.7))
+                .position(x: 570, y: 1090)
+        }
+        .frame(width: 2048, height: 1535)
+        .background(.white)
     }
 }
 
 #Preview {
-    SplashScreen()
+    SplashView()
 }
